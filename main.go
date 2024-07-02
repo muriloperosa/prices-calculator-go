98ccc68efd44b9a111f3a39a372f62d6cb39bb94
Murilo Perosa <perosamurilo@gmail.com>
2024-07-02 10:44:13 -0300

    feat: start the base program.

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 1.0, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+
+		taxIncludedPrices := make([]float64, len(prices))
+
+		for priceI, price := range prices {
+			taxIncludedPrices[priceI] = price * (1 + taxRate)
+		}
+
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(result)
 }

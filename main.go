14046935c231d4f58957240ef4b22710a2b0a47e
Murilo Perosa <perosamurilo@gmail.com>
2024-07-02 11:00:07 -0300

    refactor: Refactored the main function to use the TaxIncludedPriceJob.

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/muriloperosa/prices-calculator-go/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 1.0, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-
-		taxIncludedPrices := make([]float64, len(prices))
-
-		for priceI, price := range prices {
-			taxIncludedPrices[priceI] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }

f833e5d755fea8ba18d317f91599d658da75f382
Murilo Perosa <perosamurilo@gmail.com>
2024-07-02 17:27:53 -0300

    refactor: Refactored input/output file structure for FileManager.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/muriloperosa/prices-calculator-go/cmdmanager"
+	"github.com/muriloperosa/prices-calculator-go/filemanager"
 	"github.com/muriloperosa/prices-calculator-go/prices"
 )
 
@@ -12,12 +12,12 @@ func main() {
 
 	for _, taxRate := range taxRates {
 		// file io
-		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		// priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
+		fm := filemanager.New("storage/input/prices.txt", fmt.Sprintf("storage/output/result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
 
 		// cmd io
-		cmd := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(*cmd, taxRate)
+		// cmd := cmdmanager.New()
+		// priceJob := prices.NewTaxIncludedPriceJob(*cmd, taxRate)
 
 		err := priceJob.Process()
 

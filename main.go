45405614f4deeff3735c373ddd8e51cb6b49d7c3
Murilo Perosa <perosamurilo@gmail.com>
2024-07-02 17:20:54 -0300

    refactor: Refactored rules to use IOManager interface.

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/muriloperosa/prices-calculator-go/filemanager"
+	"github.com/muriloperosa/prices-calculator-go/cmdmanager"
 	"github.com/muriloperosa/prices-calculator-go/prices"
 )
 
@@ -11,8 +9,14 @@ func main() {
 	taxRates := []float64{0, 0.07, 1.0, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
+		// file io
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
+
+		// cmd io
+		cmd := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(*cmd, taxRate)
+
 		priceJob.Process()
 	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/muriloperosa/prices-calculator-go/conversion"
-	"github.com/muriloperosa/prices-calculator-go/filemanager"
+	"github.com/muriloperosa/prices-calculator-go/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager         filemanager.FileManager `json:"-"`
-	TaxRate           float64                 `json:"tax_rate"`
-	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -46,7 +46,7 @@ func (job *TaxIncludedPriceJob) Process() {
 	job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(fm iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
 		IOManager:   fm,
 		TaxRate:     taxRate,
